6b8ad5b2db3868e09889139fdd9dc769d925bd90
Julio_Cesar_Pariona_Ventura <Julio_P@hotmail.com>
2022-02-06 12:38:15 -0500

    se agrega el .catch()

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ const crud = require("./test");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-crud().then(products => {
-  app.get(`/`, (req, res) => {
-    res.send(`
-      <h1 style="color:blue;">Bienvenidos al servidor express</h1>
-      <p>Puede visitar las siguientes rutas:</p>
-      <ul>
-        <li>
-          <p>Productos: <a href="https://clase-06-parionaventurajuliocesar.glitch.me/productos">https://clase-06-parionaventurajuliocesar.glitch.me/productos</a></p>
-        </li>
-        <li>
-          <p>Productos random: <a href="https://clase-06-parionaventurajuliocesar.glitch.me/productorandom">https://clase-06-parionaventurajuliocesar.glitch.me/productorandom</a></p>
-        </li>
-      </ul>
-    `);
-  });
-  app.get(`/productos`, (req, res) => {
-    res.send(products);
-  });
-  app.get(`/productoRandom`, (req, res) => {
-    let indexRandom = Math.floor(Math.random() * products.length); // Genera un número random entero entre 0 y el número de objetos dentro del array.
-    res.send(products[indexRandom]);
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Servidor activo y escuchando en el puerto: ${PORT}`);
-  });
-});
+crud()
+  .then(products => {
+    app.get(`/`, (req, res) => {
+      res.send(`
+        <h1 style="color:blue;">Bienvenidos al servidor express</h1>
+        <p>Puede visitar las siguientes rutas:</p>
+        <ul>
+          <li>
+            <p>Productos: <a href="https://clase-06-parionaventurajuliocesar.glitch.me/productos">https://clase-06-parionaventurajuliocesar.glitch.me/productos</a></p>
+          </li>
+          <li>
+            <p>Productos random: <a href="https://clase-06-parionaventurajuliocesar.glitch.me/productorandom">https://clase-06-parionaventurajuliocesar.glitch.me/productorandom</a></p>
+          </li>
+        </ul>
+      `);
+    });
+    app.get(`/productos`, (req, res) => {
+      res.send(products);
+    });
+    app.get(`/productoRandom`, (req, res) => {
+      let indexRandom = Math.floor(Math.random() * products.length); // Genera un número random entero entre 0 y el número de objetos dentro del array.
+      res.send(products[indexRandom]);
+    });
+    
+    app.listen(PORT, () => console.log(`Servidor activo y escuchando en el puerto: ${PORT}`))
+  })
+  .catch(err => {
+    app.get(`/*`, (req, res) => {
+      res.send(`
+        <p>Lo sentimos, ocurrió un error:</p>
+        <p>${err.message}</p>
+      `);
+    });
+    app.listen(PORT, () => console.log(`Se detectó un problema en el puerto: ${PORT}`));
+  })
